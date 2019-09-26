044ed61483f992f65709990308199364c6277857
ocastillo711 <#Ucsbecon12>
2019-09-26 10:25:06 -0700

    Omar-Applayout created

--- app/src/main/java/com/example/internationaltriangle/MainActivity.java
+++ app/src/main/java/com/example/internationaltriangle/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.internationaltriangle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
