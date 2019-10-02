0fc96ef91e2ad1c1ded783691e078df03f63ba88
tahir-u <tahiru1994@outlook.com>
2019-10-02 01:57:23 -0400

    Add Spanish / English language support toggling * Update layout widths for labels * Update logic for language switch toggle

--- app/src/main/java/com/example/internationaltriangle/MainActivity.java
+++ app/src/main/java/com/example/internationaltriangle/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.internationaltriangle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.CompoundButton;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,12 +15,17 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonClick;
+    private Switch languageSwitch;
     private TextView sideOne;
     private TextView sideTwo;
     private TextView sideThree;
     private TextView results;
+    private TextView labelOne;
+    private TextView labelTwo;
+    private TextView labelThree;
     private HashMap<String, String> englishPhrases;
     private HashMap<String, String> spanishPhrases;
+    private HashMap<String, String> currentPhrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Add button handler/listener
         this.buttonClick = findViewById(R.id.generateButton);
         this.buttonClick.setOnClickListener(MainActivity.this);
-        this.buttonClick.setInputType(InputType.TYPE_NULL);
+
+        // Add switch handler/listener
+        this.languageSwitch = findViewById(R.id.languageSwitch);
 
         // Add text handlers/listeners
         this.sideOne = findViewById(R.id.input1);
@@ -38,6 +46,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         this.sideThree = findViewById(R.id.input3);
 
+        // Add label handlers
+        this.labelOne = findViewById(R.id.labelSide1);
+        this.labelTwo = findViewById(R.id.labelSide2);
+        this.labelThree = findViewById(R.id.labelSide3);
+
         // Add results handler
         this.results = findViewById(R.id.resultsTextBox);
 
@@ -45,10 +58,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.englishPhrases = this.generateEnglishPhrases();
         this.spanishPhrases = this.generateSpanishPhrases();
 
+        this.currentPhrases = this.generateEnglishPhrases(); // English by default
+
+        this.languageSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                String label = b ? "Spanish" : "English";
+                compoundButton.setText(label);
+            }
+        });
     }
 
     private HashMap<String, String> generateEnglishPhrases() {
         HashMap<String, String> result = new HashMap();
+
+        result.put("side 1", "Side 1:");
+        result.put("side 2", "Side 2:");
+        result.put("side 3", "Side 3:");
 
         result.put("equilateral", "Equilateral Triangle.");
         result.put("isosceles", "Isosceles Triangle.");
@@ -66,6 +92,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private HashMap<String, String> generateSpanishPhrases() {
         HashMap<String, String> result = new HashMap();
 
+        result.put("side 1", "Lado 1:");
+        result.put("side 2", "Lado 2:");
+        result.put("side 3", "Lado 3:");
+
         result.put("equilateral", "Triángulo equilátero.");
         result.put("isosceles", "Triángulo isósceles.");
         result.put("scalene", "Triángulo escaleno.");
@@ -80,8 +110,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return result;
     }
 
+    private void setCurrentPhrases(String language) {
+        switch (language) {
+            case "Spanish":
+                this.currentPhrases = this.generateSpanishPhrases();
+                break;
+            default:
+                this.currentPhrases = this.generateEnglishPhrases();
+                break;
+        }
+    }
+
+    private void setLabels() {
+        this.labelOne.setText(this.currentPhrases.get("side 1"));
+        this.labelTwo.setText(this.currentPhrases.get("side 2"));
+        this.labelThree.setText(this.currentPhrases.get("side 3"));
+    }
+
     public void onClick(View v) {
         if (v == this.buttonClick) {
+            String language = this.languageSwitch.getText().toString();
+            this.setCurrentPhrases(language);
+            this.setLabels();
 
             String sideOneString = this.sideOne.getText().toString();
             String sideTwoString = this.sideTwo.getText().toString();
@@ -96,26 +146,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                     if (Double.compare(sideOne, 0) == 0 || Double.compare(sideTwo, 0) == 0 || Double.compare(sideThree, 0) == 0) {
                         this.results.clearComposingText();
-                        this.results.setText("Goodbye!");
+                        this.results.setText(this.currentPhrases.get("goodbye"));
                     } else {
                         String triangleType = this.getTriangleTypes(sideOne, sideTwo, sideThree);
 
                         this.results.clearComposingText();
-                        this.results.setText(triangleType);
+                        this.results.setText(this.currentPhrases.get(triangleType));
                     }
                 } catch (Exception e) {
                     this.results.clearComposingText();
-                    this.results.setText("One or more triangle sides were invalid.");
+                    this.results.setText(this.currentPhrases.get("invalid sides"));
                 }
 
             } else {
 
                 if (sideOneString.contains("0") || sideTwoString.contains("0") || sideThreeString.contains("0")) {
                     this.results.clearComposingText();
-                    this.results.setText("Goodbye!");
+                    this.results.setText(this.currentPhrases.get("goodbye"));
                 } else {
                     this.results.clearComposingText();
-                    this.results.setText("One or more sides are missing for the triangle type calculation.");
+                    this.results.setText(this.currentPhrases.get("missing sides"));
                 }
             }
 
@@ -127,9 +177,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Check for negative values
         if (sideOne <= 0 || sideTwo <= 0 || sideThree <= 0) {
-            return "One or more triangle sides were invalid.";
+            return "invalid sides";
         } else if ((sideOne + sideTwo < sideThree) || (sideOne + sideThree < sideOne) || (sideTwo + sideThree < sideOne)) {
-            return "Invalid triangle (the inputs do not form a triangle).";
+            return "missing sides";
         }
 
         boolean isEquilateral = Double.compare(sides[0], sides[1]) == 0 &&
@@ -139,11 +189,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Double.compare(sides[0], sides[2]) != 0;
 
         if (isEquilateral) {
-            return "Equilateral triangle";
+            return "equilateral";
         } else if (isScalene) {
-            return "Scalene triangle";
+            return "scalene";
         } else {
-            return "Isosceles triangle";
+            return "isosceles";
         }
     }
 
